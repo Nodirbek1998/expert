c30385b19ca0ebec96552a72b6f80e71a36c23be
Nodirbek Mamadaliyev <nodirbekmamadaliyev@gmail.com>
2020-11-10 14:22:34 +0300

    first commit

---
+++ app-expert-server/src/main/java/uz/appexpertserver/AppExpertServerApplication.java
@@ -0,0 +1,13 @@
+package uz.appexpertserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppExpertServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppExpertServerApplication.class, args);
+	}
+
+}
---
+++ app-expert-server/src/test/java/uz/appexpertserver/AppExpertServerApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.appexpertserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppExpertServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
